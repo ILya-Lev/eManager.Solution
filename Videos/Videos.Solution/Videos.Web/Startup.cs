b10b7fceaee3f818929c1a689b573ba6ae012d86
illia.levandovskyi <lis2889@gmail.com>
2018-02-17 13:16:59 +0200

    boilerplate for mvc4 solution + updated packages and enabled db migrations

---
+++ Videos/Videos.Solution/Videos.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Videos.Web.Startup))]
+namespace Videos.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
