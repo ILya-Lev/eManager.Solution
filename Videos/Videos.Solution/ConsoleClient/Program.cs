6955b989ad6353187617dd3ca5296555045be9a0
illia.levandovskyi <lis2889@gmail.com>
2018-02-19 17:50:52 +0200

    added console client with xml content negotiation

---
+++ Videos/Videos.Solution/ConsoleClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+
+namespace ConsoleClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new HttpClient();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+			var response = client.GetAsync(new Uri(" http://localhost:50852/api/videos")).Result;
+
+			if (response.IsSuccessStatusCode)
+			{
+				var doc = XDocument.Parse(response.Content.ReadAsStringAsync().Result);
+				var ns = (XNamespace)"http://schemas.datacontract.org/2004/07/Videos.Web.Models";
+
+				foreach (var title in doc.Descendants(ns + "Title"))
+				{
+					Console.WriteLine(title.Value);
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
